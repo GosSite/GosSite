1a0fed8ddb867562f17fa15041f807442aca6db9
Witcher-MTM <phantomit228@gmail.com>
2024-04-24 12:57:42 +0300

    Update PDFViewer.jsx

--- src/components/PdfViewer/PDFViewer.jsx
+++ src/components/PdfViewer/PDFViewer.jsx
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
+
 const PDFViewer = () => {
     useEffect(() => {
         pdfjs.GlobalWorkerOptions.workerSrc = new URL(
             'pdfjs-dist/build/pdf.worker.min.js',
             import.meta.url,
         ).toString();
-    }, [])
+        loadAgreedState();
+    }, []);
+
+    const loadAgreedState = async () => {
+        const hasAgreed = await localStorage.getItem('hasAgreed');
+        if (hasAgreed) {
+            setIsVisibleDoc(false);
+        }else{
+            setIsVisibleDoc(true)
+        }
+    };
+
     const [numPages, setNumPages] = useState();
     const [pageNumber, setPageNumber] = useState(1);
+    const [isVisibleDoc, setIsVisibleDoc] = useState(true);
+
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
-    const handleAgree = () => {
-        alert('Вы подтверждаете ознакомление с документом.');
-        // Дополнительные действия по подтверждению ознакомления
+
+    const handleAgree = async () => {
+        await localStorage.setItem('hasAgreed', true);
+        setIsVisibleDoc(false);
     };
 
     const handlePrevious = () => {
@@ -31,27 +46,89 @@ const PDFViewer = () => {
     };
 
     return (
-        <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.8)', zIndex: 9999 }}>
-            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-                <div style={{ height: '80vh', overflowY: 'scroll' }}>
-                    <Document file="../../pdf/agreec.pdf" inputProps={{ style: { color: 'white' } }} onLoadSuccess={onDocumentLoadSuccess}>
-                        <Page pageNumber={pageNumber} />
-                    </Document>
+        <>
+            {isVisibleDoc && (
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                        zIndex: 9999,
+                    }}
+                >
+                    <div
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            textAlign: 'center',
+                        }}
+                    >
+                        <div style={{ height: '80vh', overflowY: 'scroll' }}>
+                            <Document
+                                file="../../pdf/agreec.pdf"
+                                inputProps={{ style: { color: 'white' } }}
+                                onLoadSuccess={onDocumentLoadSuccess}
+                            >
+                                <Page pageNumber={pageNumber} />
+                            </Document>
+                        </div>
+                        <div style={{ backgroundColor: '#fff' }}>
+                            <p>
+                                Page {pageNumber} of {numPages}
+                            </p>
+                            <button
+                                style={{
+                                    padding: '5px 20px',
+                                    fontSize: '20px',
+                                    backgroundColor: '#fff',
+                                    border: 'none',
+                                    borderRadius: '5px',
+                                    cursor: 'pointer',
+                                }}
+                                onClick={handlePrevious}
+                                disabled={pageNumber <= 1}
+                            >
+                                &lt;
+                            </button>
+                            <button
+                                style={{
+                                    padding: '5px 20px',
+                                    fontSize: '20px',
+                                    backgroundColor: '#fff',
+                                    border: 'none',
+                                    borderRadius: '5px',
+                                    cursor: 'pointer',
+                                }}
+                                onClick={handleNext}
+                                disabled={pageNumber >= numPages}
+                            >
+                                &gt;
+                            </button>
+                        </div>
+                        <div></div>
+                        <button
+                            onClick={handleAgree}
+                            style={{
+                                marginTop: '20px',
+                                padding: '10px 20px',
+                                fontSize: '16px',
+                                backgroundColor: '#007bff',
+                                border: 'none',
+                                borderRadius: '5px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            Я ознакомлен
+                        </button>
+                    </div>
                 </div>
-                <div style={{backgroundColor:"#fff"}}>
-                <p>
-                    Page {pageNumber} of {numPages}
-                </p>
-                <button style={{ padding: '5px 20px', fontSize: '20px', backgroundColor: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }} onClick={handlePrevious} disabled={pageNumber <= 1}>&lt;</button>
-                <button style={{ padding: '5px 20px', fontSize: '20px', backgroundColor: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }} onClick={handleNext} disabled={pageNumber >= numPages}>&gt;</button>
-                </div>
-                <div>
-                </div>
-                <button onClick={handleAgree} style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px', backgroundColor: '#007bff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-                    Я ознакомлен
-                </button>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
